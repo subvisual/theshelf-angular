f2a492211f7b4c85349a856f795a7c6263deb12c
Bruno Azevedo <azevedo.252@gmail.com>
2014-12-21 22:26:37 +0000

    search-bar directive unit test

--- test/unit/components/search_bar/search_bar_spec.js
+++ test/unit/components/search_bar/search_bar_spec.js
@@ -1,43 +1,38 @@
 'use strict';
 
 describe('Unit: SearchBar SearchBar', function() {
-  // NOT SURE THIS DOESN'T WORK
+  var element;
+  var outerScope;
 
-  // var element;
-  // var outerScope;
-  //
-  // beforeEach(module('theshelf.search_bar'));
-  // beforeEach(module('karma.templates'));
-  //
-  // beforeEach(inject(function($rootScope, $compile) {
-  //   element = angular.element('<search-bar on-user-input="userInputCallback()"></search-bar>');
-  //
-  //   outerScope = $rootScope;
-  //   $compile(element)(outerScope);
-  //
-  //   outerScope.$digest();
-  // }));
-  //
-  // describe('user input callback', function() {
-  //   var callbackSpy;
-  //
-  //   beforeEach(function() {
-  //     callbackSpy = sinon.spy();
-  //     outerScope.$apply(function() {
-  //       outerScope.userInputCallback = callbackSpy;
-  //     });
-  //   });
-  //
-  //   describe('when the user inputs something', function() {
-  //     beforeEach(function() {
-  //       var event = document.createEvent('UIEvents');
-  //       event.initUIEvent('input', true, true, window, 1);
-  //       element[0].dispatchEvent(event);
-  //     });
-  //
-  //     it('should be called', function() {
-  //       expect(callbackSpy.called).to.be.true;
-  //     });
-  //   });
-  // });
+  beforeEach(module('theshelf.search_bar'));
+  beforeEach(module('karma.templates'));
+
+  beforeEach(inject(function($rootScope, $compile) {
+    element = angular.element('<search-bar on-user-input="userInputCallback()"></search-bar>');
+
+    outerScope = $rootScope.$new();
+
+    element = $compile(element)(outerScope);
+
+    outerScope.$digest();
+  }));
+
+  describe('user input callback', function() {
+    var callbackSpy;
+
+    beforeEach(function() {
+      callbackSpy = sinon.spy();
+      outerScope.$apply(function() {
+        outerScope.userInputCallback = callbackSpy;
+      });
+    });
+
+    describe('when the user inputs something', function() {
+      it('should be called', function() {
+        var isolated = element.isolateScope();
+        isolated.ctrl.onUserInput();
+        expect(callbackSpy.called).to.be.true;
+      });
+    });
+  });
 });
