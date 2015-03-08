d72f03014eb46897772772760742a2da2400f319
Bruno Azevedo <azevedo.252@gmail.com>
2015-03-08 21:27:37 +0000

    Added note to tests

--- test/unit/components/search_bar/search_bar_ctrl.spec.js
+++ test/unit/components/search_bar/search_bar_ctrl.spec.js
@@ -9,6 +9,7 @@ describe('Unit: theshelf.components.search_bar SearchBarCtrl', function() {
     inject(function($controller) {
       // testing `bindToController: true`
       // see: https://github.com/angular/angular.js/issues/9425
+      // TODO: LATEST NEWS: https://github.com/angular/angular.js/commit/d02d0585a086ecd2e1de628218b5a6d85c8fc7bd
       ctrlFn = $controller('SearchBarCtrl', {}, true);
     });
   });
